eca306afaf051a311d3f9cebb23eec2b911cc955
kmcushing <48609545+kmcushing@users.noreply.github.com>
2024-05-15 12:15:10 -0500

    update user representation

--- user_representation.py
+++ user_representation.py
@@ -1,6 +1,4 @@
-# TODO: implement a simple user representation
 # could initially just be a json with most common topics (and some measure of their frequency) along with a list of domains they have insider info on
-import json
 import jsons
 
 # TODO: define an enum for topics based on how we are extracting them
@@ -12,9 +10,11 @@ class User:
     # TODO: may want to also store user sentiment with topics
     # TODO: better representation for user domains
     # TODO: use some sort of decay for user's frequent topics with a decay rate for each message?
-    def __init__(self, insider_domains=None, topic_frequencies=None):
+    def __init__(self, id, insider_domains=None, topic_frequencies=None):
+        self.id = id
         self.insider_domains = insider_domains if insider_domains else {}
         self.topic_frequencies = topic_frequencies if topic_frequencies else {}
+        self.save()
 
     def disscussed_topic(self, topic):
         # adds to discussion count of topic in user representation
@@ -22,13 +22,16 @@ class User:
             self.topic_frequencies[topic] += 1
         else:
             self.topic_frequencies[topic] = 1
+        self.save()
 
     def add_insider_domain(self, domain):
         # adds insider domain for user
         self.insider_domains[domain] = 1
+        self.save()
 
     def remove_insider_domain(self, domain):
         self.insider_domains.pop(domain, None)
+        self.save()
 
     def __str__(self):
         return f"Insider Domains: {self.insider_domains}\nTopic Frequencies: {self.topic_frequencies}"
@@ -46,10 +49,20 @@ class User:
                 s += f"{d}\n"
         return s
 
+    def save(self):
+        f = open(f"data/user_representations/{self.id}.json", "w")
+        f.write(jsons.dumps(self))
+
 
 # load user representation
-# f = open("data/user_representations/users.json", "r")
-# data = json.loads(f.read())
-# user1 = jsons.load(data["user1"], User)
-# print(user1)
-# print(user1.to_prompt())
+def load_user(id):
+    # TODO: may want to remove hard-coded path
+    try:
+        f = open(f"data/user_representations/{id}.json", "r")
+        return jsons.loads(f.read(), User)
+    except:
+        return User(id)
+
+
+user1 = load_user("user1")
+print(user1)
